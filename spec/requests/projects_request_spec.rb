0a9e217c559ab8bc10d514caf0f2f50b7f367800
Asef90 <asef.dag90@gmail.com>
2020-08-01 01:57:19 +0300

    add tests for todos

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -6,23 +6,21 @@ RSpec.describe 'Projects', type: :request do
   let!(:projects) { create_list(:project, 3, :with_todos) }
 
   describe 'GET /projects' do
-    before do
-      get '/projects', headers: headers
-    end
+    before { get '/projects', headers: headers }
 
     it 'returns http success' do
       expect(response).to be_successful
     end
 
     context 'response content' do
-      let(:json_response) { JSON.parse(response.body)['projects'] }
+      let(:projects_response) { json_response['projects'] }
 
       it 'returns all public project attributes' do
-        expect(json_response.first.keys).to match_array(['id', 'title', 'todos'])
+        expect(projects_response.first.keys).to match_array(['id', 'title', 'todos'])
       end
 
       it 'returns all existing projects' do
-        expect(json_response.count).to eq(projects.count)
+        expect(projects_response.count).to eq(projects.count)
       end
     end
   end
--- spec/requests/todos_request_spec.rb
+++ spec/requests/todos_request_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
+  let(:headers) { { 'ACCEPT' => 'application/json' } }
+  let!(:project) { create(:project) }
+  let!(:todo) { create(:todo, is_completed: false, project: project) }
+  let(:todo_response) { json_response['todo'] }
 
+  describe 'POST /todos' do
+    context 'project already exists' do
+      before { post '/todos', params: { todo: attributes_for(:todo), project: { title: project.title } },
+                              headers: headers }
+
+      it 'returns todo response for specified project' do
+        expect(todo_response['project_id']).to eq project.id
+      end
+
+      it 'returns all public project attributes' do
+        expect(todo_response.keys).to match_array(['id', 'text', 'is_completed', 'project_id'])
+      end
+
+      it 'returns http success' do
+        expect(response).to be_successful
+      end
+    end
+
+    context 'project does not exist' do
+      before { post '/todos', params: { todo: attributes_for(:todo), project: { title: 'Another project' } },
+                              headers: headers }
+
+      it 'returns todo response for new project' do
+        expect(todo_response['project_id']).to_not eq project.id
+      end
+
+      it 'returns all public project attributes' do
+        expect(todo_response.keys).to match_array(['id', 'text', 'is_completed', 'project_id'])
+      end
+
+      it 'returns http success' do
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'PATCH /projects/:project_id/todos/:id' do
+    before { patch "/projects/#{project.id}/todos/#{todo.id}", params: { todo: { is_completed: true } },
+                                                               headers: headers }
+
+    it 'returns http success' do
+      expect(response).to be_successful
+    end
+
+    context 'response content' do
+      it 'returns all public project attributes' do
+        expect(todo_response.keys).to match_array(['id', 'text', 'is_completed', 'project_id'])
+      end
+
+      it 'returns todo with completed status' do
+        expect(todo_response['is_completed']).to be_truthy
+      end
+    end
+  end
 end
